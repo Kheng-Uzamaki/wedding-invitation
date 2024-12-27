6e5c03d4102356689aabdfa88473ac4a315cab4a
Kheng <khenguzamiki1@gmail.com>
2024-12-27 11:52:57 +0700

    completed by Thai

---
+++ js/slide.js
@@ -0,0 +1,28 @@
+var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    loop:true,
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    autoplay: {
+      delay: 3500, // 3 seconds
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      renderBullet: function (index, className) {
+        if (index < 8) {
+          return '<span class="' + className + '">' + '</span>';
+        }
+        return ''; // Do not render bullets beyond the first 8
+      },
+    },
+  });
